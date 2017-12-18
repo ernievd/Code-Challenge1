b2ac1e846e5fbc6e60fd2ac1cdc037a5bb548bb9
ernievd <ernievd@gmail.com>
2017-12-18 14:20:51 -0600

    rev2: added more

--- scripts.js
+++ scripts.js
@@ -1,12 +1,26 @@
 console.log('JS loaded');
-
+var clicks = 0;
 // When the DOM is loaded, our code in the onReady function will run.
 $(document).ready(function(){
 
   $('#generateButton').on('click', Main);
+  $('#mainBody').on('click', 'swapButton' , doToggle(event));
 
 });
 
 function Main() {
-  $('#generateButton').append("<div></div>");
+  // $('#dataTableBody').on('click', '.deleteButton', RemoveRow);
+  $('#mainBody').append("<div class=divClass id=divId> </div>");
+  clicks++;
+  $('#divId').append("<p id=p1></p>");
+  $('#p1').text(clicks);
+
+  $('#divId').append("<button type=\"button\" id=\"swapButton\">Swap</button>");
+  $('#divId').append("<button type=\"button\" id=\"deleteButton\">Delete</button>");
 } // END: Main
+
+function doToggle(event) {
+    $(event).css("background-color", "yellow");
+
+    //$(this).toggleClass('');
+} // END: doToggle
